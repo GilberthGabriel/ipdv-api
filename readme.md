ffa4ada02e040f99aae835fcc6426ceb0cde0094
Gilberth Gabriel <dev.gilberth@gmail.com>
2022-09-26 13:22:01 -0300

    add config files

---
+++ readme.md
@@ -0,0 +1,9 @@
+# IPDV-API
+Repositório da API do sistema de gerenciamento IPDV.
+
+# Instruções
+Para iniciar o projeto, basta seguir as seguintes instruções:
+1. Rodar comando `npm install`
+2. Rodar comando `npm start`
+
+Por padrão será utilizado um banco de dados em nuvem, porém é possível alterar esse banco de dados através das variáveis de ambiente, basta inserir os dados de acesso no arquivo `.env`
--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize({
-  dialect: 'postgres',
-  database: 'lejhwkyp',
-  username: 'lejhwkyp',
-  password: 'AW0jy2Bpp71OiLe7GLeZxmnifDZzWUjf',
-  host: 'jelani.db.elephantsql.com',
+  dialect: process.env.DB_DIALECT || 'postgres',
+  database: process.env.DB_NAME || 'lejhwkyp',
+  username: process.env.DB_USERNAME || 'lejhwkyp',
+  password: process.env.DB_PASSWORD || 'AW0jy2Bpp71OiLe7GLeZxmnifDZzWUjf',
+  host: process.env.DB_HOST || 'jelani.db.elephantsql.com',
 });
 
 sequelize.sync({ alter: true });
